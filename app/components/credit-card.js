a51752a70ddbe3e46ce6bc90660ab6b082451e32
NorbertNader <norbert.nader@gmail.com>
2018-04-29 12:52:26 +0200

    Finish second task

--- app/components/credit-card.js
+++ app/components/credit-card.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   setCurrentDate: Ember.on('init', function(){
+    let date = new Date();
     //override `today` property for integration tests
     if (!this.get("today")) {
-      this.set('today', new Date());
+
+      this.set('today', date.getTime());
     }
   }),
 
@@ -14,12 +16,16 @@ export default Ember.Component.extend({
   classNames: ['credit-card', 'text-left'],
 
   //binds class name of 'is-expire' when the `isExpired` property is true.
-  classNameBindings: ["isExpired"],
+  classNameBindings: ["isExpired:is-expired"],
 
   //returns true if `card` is expired, otherwise returns false.
   isExpired: Ember.computed("card.expirationMonth", "card.expirationYear", function() {
     let card = this.get("card");
     let today = this.get("today");
+    let expiresOn = new Date(card.expirationYear, card.expirationMonth).getTime();
+
+    if (expiresOn < today) return true;
+
     return false;
   })
 /*
